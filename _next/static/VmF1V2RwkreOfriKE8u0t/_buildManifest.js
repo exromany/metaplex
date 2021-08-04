281db26868183bfcb8a740d0f1345dbd443cb717
exromany <exromany@gmail.com>
2021-08-04 21:28:28 +0300

    Updates

---
+++ _next/static/VmF1V2RwkreOfriKE8u0t/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-e0962ca4d326c9fb112f.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
